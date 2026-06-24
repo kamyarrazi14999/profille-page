f0a2f650559d0f96aeff12451bba763041a250a6
kamyarrazi14999 <kami6535@gmail.com>
2026-06-25 00:06:29 +0330

    new index

--- script.js
+++ script.js
@@ -59,13 +59,38 @@ backProfileBtn.onclick = () => {
           pages[pageNumber].style.zIndex = 10 + index;
         }, 500);
       },
-      (index + 1) * 200,
+      (index + 1) * 200 + 100,
     );
   });
 };
 //opening animation
 const coverRight = document.querySelector(".cover.cover-right");
+const coverLeft = document.querySelector(".book-page.page-left");
+
 // opening animation cover right animation
 setTimeout(() => {
   coverRight.classList.add("turn");
 }, 2100);
+setTimeout(() => {
+  coverRight.style.zIndex = -1;
+}, 2800);
+// opening animation cover left animation
+setTimeout(() => {
+  coverLeft.style.zIndex = 20;
+}, 3200);
+// opening animation (all page right animation)
+
+pages.forEach((_, index) => {
+  setTimeout(
+    () => {
+      reverseIndex();
+      pages[pageNumber].classList.remove("turn");
+
+      setTimeout(() => {
+        reverseIndex();
+        pages[pageNumber].style.zIndex = 10 + index;
+      }, 500);
+    },
+    (index + 1) * 200 + 2100,
+  );
+});
